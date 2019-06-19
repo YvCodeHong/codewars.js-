a296e809107b3abe2ca2fd0e476a24ca105e6b36
Yvonne Tang <ytang604@gmail.com>
2019-06-19 14:54:53 +0100

    complete lostwithoutMap

---
+++ lostwithoutMap.js
@@ -0,0 +1,17 @@
+//Given an array of integers, return a new array with each value 
+//doubled 
+
+// Breaking the problem down: 
+// User enters an array of numbers e.g [2, 5, 8, 9]
+// Output should  [4, 10, 16, 18]
+// input array mulitply by 2 
+
+
+const maps = x => x.map(num => num * 2); 
+
+
+
+
+function maps(x) {
+    return x.map(n => n * 2); 
+}
