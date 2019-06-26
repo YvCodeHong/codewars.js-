b13596893f729e7de9be6f09899149482f9e9050
Yvonne Tang <ytang604@gmail.com>
2019-06-26 16:31:06 +0100

    complete girlfriend

---
+++ 8kyu /girlFriend.js
@@ -0,0 +1,5 @@
+//Write a function isMyGirlfriendRight that always return true
+//As with any enduring relationship, your function should be resilent 
+//to all manners of arguments and outside influences. 
+
+const isMyGirlfriendRight = () => true; 
