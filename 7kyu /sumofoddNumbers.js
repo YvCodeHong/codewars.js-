ffacef4f40054a066f3b96ed440469631d87f7f1
Yvonne Tang <ytang604@gmail.com>
2019-07-19 20:33:34 +0100

    add codewar 7

---
+++ 7kyu /sumofoddNumbers.js
@@ -0,0 +1,26 @@
+//Sum of odd Numbers 
+
+//Given the triangle of consecutive odd numbers: 
+//   1
+// 3     5
+// 7     9    11
+// 13    15    17    19
+// 21    23    25    27    29
+// ...
+
+//Calculate the row sums of this triangle from the row index
+//(starting at index 1) e.g: 
+
+//rowSumOddNumbers(1); //1
+//rowSumOddNumbers(2); //3 + 5 = 8
+
+
+function rowSumOddNumbers(n) {
+    let start = n * (n - 1) + 1;
+    let sum = 0;
+
+    for (let i = 0; i < n; i++) {
+        sum += start + (i * 2);
+    } 
+}
+
