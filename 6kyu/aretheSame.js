adb544872ae88c7d445f32fc42a689fcc947d89c
Yvonne Tang <ytang604@gmail.com>
2019-10-02 11:02:23 +0100

    add short version

--- 6kyu/aretheSame.js
+++ 6kyu/aretheSame.js
@@ -18,3 +18,11 @@ function comp(array1, array2) {
         }
         return true; 
 }
+
+//short version
+
+function comp(array1, array1) {
+    if(array1 == null || array2 == null) return false; 
+    array1.sort((a, b) => a - b); array2.sort((a, b) => a -b);
+    return array1.map(v => v * v).every((v, i) => v == array2[i]);
+}
